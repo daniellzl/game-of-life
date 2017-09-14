e290cf4656f02297640d6e9efaac29cf7f6d3ef4
daniellzl <mr.daniel.liang@gmail.com>
2017-09-13 22:03:32 -0700

    cleaned up indentation

--- scripts.js
+++ scripts.js
@@ -17,14 +17,13 @@ const UPDATETIME = 1500; // time it takes to advanced one generation in game of 
 ██   ██ ███████ ██   ██ ██████  ███████ ██   ██
 */
 
+// header displayed on page
 const Header = (props) => {
-	// header displayed on page
-
-    return (
-        <div>
-            <h1>Game of Life</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Game of Life</h1>
+    </div>
+  );
 }
 
 /*
@@ -35,14 +34,13 @@ const Header = (props) => {
  ██████  ███████ ██   ████ ██████  ██ ███████ ██
 */
 
+// displays the current generation
 const GenerationDisplay = (props) => {
-	// displays the current generation
-
-    return (
-        <div>
-            <h4>Current Generation: {props.currentGen}</h4>
-        </div>
-    );
+  return (
+    <div>
+      <h4>Current Generation: {props.currentGen}</h4>
+    </div>
+  );
 }
 
 /*
@@ -53,21 +51,22 @@ const GenerationDisplay = (props) => {
  ██████  ██   ██ ██ ██████
 */
 
+// Return entire grid representing current state of game
 const Grid = (props) => {
-	// Return entire grid representing current state of game
 
-    const cells = props.currentGrid.map((row, index) => {
-        return (<GridRow key={index} row={row} rowIndex={index} onCellClick={props.onCellClick}/>);
-    });
-    return (
-        <div>
-            <table>
-                <tbody>
-                    {cells}
-                </tbody>
-            </table>
-        </div>
-    );
+  const cells = props.currentGrid.map((row, index) => {
+    return (<GridRow key={index} row={row} rowIndex={index} onCellClick={props.onCellClick}/>);
+  });
+
+  return (
+    <div>
+      <table>
+        <tbody>
+          {cells}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 /*
@@ -78,21 +77,22 @@ const Grid = (props) => {
  ██████  ██   ██ ██ ██████  ██   ██  ██████   ███ ███
 */
 
+// Return grid row representing current state of game
 const GridRow = (props) => {
-	// Return grid row representing current state of game
 
-    const row = props.row.map((cell, index) => {
-        return (
-            <td className={(cell === true)
-                ? "alive"
-                : "dead"} key={index} onClick={() => props.onCellClick(props.rowIndex, index)}></td>
-        );
-    });
+  const row = props.row.map((cell, index) => {
     return (
-        <tr>
-            {row}
-        </tr>
+      <td className={(cell === true)
+        ? "alive"
+        : "dead"} key={index} onClick={() => props.onCellClick(props.rowIndex, index)}></td>
     );
+  });
+
+  return (
+    <tr>
+      {row}
+    </tr>
+  );
 }
 
 /*
@@ -103,16 +103,15 @@ const GridRow = (props) => {
 ██████   ██████     ██       ██     ██████  ██   ████ ███████
 */
 
+// Buttons that start, stop, and clear the game
 const Buttons = (props) => {
-	// Buttons that start, stop, and clear the game
-
-    return (
-        <div>
-            <button type="button" className="start" onClick={props.playGame}>Start</button>
-            <button type="button" className="stop" onClick={props.stopGame}>Stop</button>
-            <button type="button" className="clear" onClick={props.clearGame}>Clear</button>
-        </div>
-    );
+  return (
+    <div>
+      <button type="button" className="start" onClick={props.playGame}>Start</button>
+      <button type="button" className="stop" onClick={props.stopGame}>Stop</button>
+      <button type="button" className="clear" onClick={props.clearGame}>Clear</button>
+    </div>
+  );
 }
 
 /*
@@ -123,17 +122,16 @@ const Buttons = (props) => {
 ██ ██   ████ ███████    ██    ██   ██  ██████   ██████    ██
 */
 
+// Instructions for using the app
 const Instructions = (props) => {
-	// Instructions for using the app
-
-    return (
-        <div>
-            <p>You can add or remove cells anytime you want!</p>
-            <p>Learn more about the Game of Life at
-                <a href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life">Wikipedia</a>
-            </p>
-        </div>
-    );
+  return (
+    <div>
+      <p>You can add or remove cells anytime you want!</p>
+      <p>Learn more about the Game of Life at
+        <a href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life">Wikipedia</a>
+      </p>
+    </div>
+  );
 }
 
 /*
@@ -144,29 +142,29 @@ const Instructions = (props) => {
 ██   ██ ██   ██ ██   ████ ██████   ██████  ██   ██ ██ ██████
 */
 
+/*
+Int -> Arr[Arr[Bool]]
+Given integer n, return an n x n 2D array populated with either true or false
+*/
 const generateRandomGrid = (size) => {
-    /*
-  	Int -> Arr[Arr[Bool]]
-  	Given integer n, return an n x n 2D array populated with either true or false
-  	*/
 
-    // result 2D array
-    let result = [];
+  // result 2D array
+  let result = [];
 
-    // populate array with true or false
-    for (var i = 0; i < size; i++) {
-        var row = [];
-        for (var j = 0; j < size; j++) {
-            if (Math.random() > 0.5) {
-                row.push(true);
-            } else {
-                row.push(false)
-            }
-        }
-        result.push(row);
+  // populate array with true or false
+  for (var i = 0; i < size; i++) {
+    var row = [];
+    for (var j = 0; j < size; j++) {
+      if (Math.random() > 0.5) {
+        row.push(true);
+      } else {
+        row.push(false)
+      }
     }
+    result.push(row);
+  }
 
-    return result;
+  return result;
 }
 
 /*
@@ -177,25 +175,25 @@ const generateRandomGrid = (size) => {
 ██████  ███████ ██   ██ ██████   ██████  ██   ██ ██ ██████
 */
 
+/*
+Int -> Arr[Arr[Bool]]
+Given integer n, return an n x n 2D array populated with false
+*/
 const generateDeadGrid = (size) => {
-    /*
-	Int -> Arr[Arr[Bool]]
-	Given integer n, return an n x n 2D array populated with false
-	*/
 
-    // result 2D array
-    let result = [];
+  // result 2D array
+  let result = [];
 
-    // populate array with false
-    for (var i = 0; i < size; i++) {
-        var row = [];
-        for (var j = 0; j < size; j++) {
-            row.push(false)
-        }
-        result.push(row);
+  // populate array with false
+  for (var i = 0; i < size; i++) {
+    var row = [];
+    for (var j = 0; j < size; j++) {
+      row.push(false)
     }
+    result.push(row);
+  }
 
-    return result;
+  return result;
 }
 
 /*
@@ -206,8 +204,8 @@ const generateDeadGrid = (size) => {
  ███ ███  ██ ███████ ███████ ███████ ██   ████   ███████
 */
 
+// Given the row index and column index of a grid along with the grid, apply the rules of game of life to cell defined by row and index to determine whether it lives or dies
 const willLive = (rowIndex, colIndex, currentGrid) => {
-	// Given the row index and column index of a grid along with the grid, apply the rules of game of life to cell defined by row and index to determine whether it lives or dies
 
 	// obtain cell
 	const cell = currentGrid[rowIndex][colIndex];
@@ -216,7 +214,7 @@ const willLive = (rowIndex, colIndex, currentGrid) => {
 	var livingNeighbours = 0;
 	const neighbourIndices = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]];
 	neighbourIndices.forEach(function(index) {
-		if ((currentGrid[rowIndex + index[0]] !== undefined) && (currentGrid[rowIndex + index[0]][colIndex + index[1]] === true)) {
+    if ((currentGrid[rowIndex + index[0]] !== undefined) && (currentGrid[rowIndex + index[0]][colIndex + index[1]] === true)) {
 			livingNeighbours += 1;
 		}
 	});
@@ -234,118 +232,113 @@ const willLive = (rowIndex, colIndex, currentGrid) => {
 */
 
 class App extends React.Component {
-	// main application
 
 	constructor(props) {
-	    super(props);
-	    this.state = {
-	        gridA: [],
-	        gridB: [],
-	        currentGen: 1,
-	        displayGrid: "A",
-	        gridSize: GRIDSIZE,
-	        gameRunning: false,
-	        updateTime: UPDATETIME
-	    }
-		this.changeCellStatus = this.changeCellStatus.bind(this);
-		this.playGame = this.playGame.bind(this)
-		this.stopGame = this.stopGame.bind(this)
-		this.clearGame = this.clearGame.bind(this)
+	  super(props);
+	  this.state = {
+	    gridA: [],
+	    gridB: [],
+	    currentGen: 1,
+	    displayGrid: "A",
+	    gridSize: GRIDSIZE,
+	    gameRunning: false,
+	    updateTime: UPDATETIME
+	  }
+	  this.changeCellStatus = this.changeCellStatus.bind(this);
+	  this.playGame = this.playGame.bind(this)
+	  this.stopGame = this.stopGame.bind(this)
+	  this.clearGame = this.clearGame.bind(this)
 	}
 
+  // when application is loaded, generate grids and start game
 	componentDidMount() {
-	    // when application is loaded, generate grids and start game
-
-	    this.setState({
-	        gridA: generateRandomGrid(this.state.gridSize),
-	        gridB: generateDeadGrid(this.state.gridSize)
-	    }, this.playGame());
+	  this.setState({
+	    gridA: generateRandomGrid(this.state.gridSize),
+	    gridB: generateDeadGrid(this.state.gridSize)
+	  }, this.playGame());
 	}
 
+  // Take the current generation grid, apply game of life rules to grid, save to next generation grid and update generation
 	advanceGeneration() {
-		// Take the current generation grid, apply game of life rules to grid, save to next generation grid and update generation
 
-	    // assign current generation grid
-	    let currentGrid = this.state.displayGrid === "A"
-	        ? this.state.gridA
-	        : this.state.gridB;
-	    let nextGrid = this.state.displayGrid === "A"
-	        ? this.state.gridB
-	        : this.state.gridA;
+    // assign current generation grid
+    let currentGrid = this.state.displayGrid === "A" ?
+      this.state.gridA :
+      this.state.gridB;
+    let nextGrid = this.state.displayGrid === "A" ?
+      this.state.gridB :
+      this.state.gridA;
 
-	    // for each cell, determine if cell lives or dies for next generation
-	    currentGrid.forEach(function(row, rowIndex) {
-	        row.forEach(function(cell, colIndex) {
+    // for each cell, determine if cell lives or dies for next generation
+    currentGrid.forEach(function(row, rowIndex) {
+      row.forEach(function(cell, colIndex) {
 
-	            // save determination in next generation grid
-	            nextGrid[rowIndex][colIndex] = willLive(rowIndex, colIndex, currentGrid);
-	        }, this);
-	    }, this);
+        // save determination in next generation grid
+        nextGrid[rowIndex][colIndex] = willLive(rowIndex, colIndex, currentGrid);
+      }, this);
+    }, this);
 
-	    // save next generation grid and update relevant generational information;
-	    if (this.state.displayGrid === "A") {
-	        this.setState({
-	            gridB: nextGrid,
-	            currentGen: this.state.currentGen + 1,
-	            displayGrid: "B"
-	        });
-	    } else {
-	        this.setState({
-	            gridA: nextGrid,
-	            currentGen: this.state.currentGen + 1,
-	            displayGrid: "A"
-	        });
-	    }
+    // save next generation grid and update relevant generational information;
+    if (this.state.displayGrid === "A") {
+      this.setState({
+        gridB: nextGrid,
+        currentGen: this.state.currentGen + 1,
+        displayGrid: "B"
+      });
+    } else {
+      this.setState({
+        gridA: nextGrid,
+        currentGen: this.state.currentGen + 1,
+        displayGrid: "A"
+      });
+    }
 	}
 
+  // Start game of life
 	playGame() {
-	    // Start game of life
-
-	    if (this.state.gameRunning === false) {
-	        this.gameInstance = setInterval(() => this.advanceGeneration(), this.state.updateTime);
-	        this.setState({gameRunning: true});
-	    }
+    if (this.state.gameRunning === false) {
+      this.gameInstance = setInterval(() => this.advanceGeneration(), this.state.updateTime);
+      this.setState({ gameRunning: true });
+    }
 	}
 
+  // Stop the game from playing out
 	stopGame() {
-	    // Stop the game from playing out
-
-	    this.setState({gameRunning: false});
-	    clearInterval(this.gameInstance);
+    this.setState({gameRunning: false});
+    clearInterval(this.gameInstance);
 	}
 
+  // Stop game from playing out and resets the board
 	clearGame() {
-		// Stop game from playing out and resets the board
-
-	    this.setState({gameRunning: false});
-	    clearInterval(this.gameInstance);
-	    const emptyGrid = this.generateDeadGrid(30);
-	    this.setState({gridA: emptyGrid, currentGen: 1, displayGrid: "A"});
+    this.setState({gameRunning: false});
+    clearInterval(this.gameInstance);
+    const emptyGrid = this.generateDeadGrid(30);
+    this.setState({gridA: emptyGrid, currentGen: 1, displayGrid: "A"});
 	}
 
+  // Takes in a cells row index and column index and changes the status of that cell (dead to live, live to dead)
 	changeCellStatus(rowIndex, colIndex) {
-		// Takes in a cells row index and column index and changes the status of that cell (dead to live, live to dead)
 
-	    // obtain current grid
-	    var currentGrid = (this.state.displayGrid === "A")
-	        ? this.state.gridA
-	        : this.state.gridB;
+    // obtain current grid
+    var currentGrid = (this.state.displayGrid === "A") ?
+      this.state.gridA :
+      this.state.gridB;
 
-	    // change status of cell
-	    currentGrid[rowIndex][colIndex] = !(currentGrid[rowIndex][colIndex]);
+    // change status of cell
+    currentGrid[rowIndex][colIndex] = !(currentGrid[rowIndex][colIndex]);
 
-	    // save grid
-	    if (this.state.displayGrid === "A") {
-	        this.setState({gridA: currentGrid});
-	    } else {
-	        this.setState({gridB: currentGrid});
-	    }
+    // save grid
+    if (this.state.displayGrid === "A") {
+      this.setState({ gridA: currentGrid });
+    } else {
+      this.setState({ gridB: currentGrid });
+    }
 	}
 
 	render() {
 		return (
 			<div>
-				<Header />
+        <Header />
 				<GenerationDisplay
 					currentGen={this.state.currentGen}
 				/>
